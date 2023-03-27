765fa726de79629ec308bda6cd2ad8391918759d
shaphat-pixel <khalshaphat@gmail.com>
2023-03-27 00:44:11 +0000

    about to deploy

--- domain_search/urls.py
+++ domain_search/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from scraper import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('name-cheap/', views.NameCheap, name='post'),
     path('domain/', views.Domain, name='post'),
     path('hover/', views.Hover, name='post')
